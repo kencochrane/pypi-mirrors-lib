c7fb3978621e06d0dd88ccfcaee903871700f4b1
Ken Cochrane <kencochrane@gmail.com>
2012-04-14 14:27:14 -0400

    Initial checkin

---
+++ README.rst
@@ -0,0 +1,6 @@
+PyPI-Mirrors
+-----------
+
+This is a simple library that will make it easier to find out the status of the PyPI mirrors.
+
+Used by http://www.pypi-mirrors.org
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+from pypimirrors import __version__
+
+setup(
+    name='pypi-mirrors',
+    version=__version__,
+    packages=[''],
+    url='http://github.com/kencochrane/pypi-mirrors-lib',
+    license='MIT',
+    author='Ken Cochrane',
+    author_email='KenCochrane@gmail.com',
+    description='pypi mirror status library'
+)
